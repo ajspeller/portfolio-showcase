7f35fb5d1c12e92b2a20070a52f3d97b2e5f028e
AJ Speller <ajspeller@gmail.com>
2018-06-26 01:13:32 -0400

    Add demo apps and webpages

--- js/init.js
+++ js/init.js
@@ -1,8 +1,18 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    // $('.carousel').carousel();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true
+  });
+});
